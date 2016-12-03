31b12a0352421a7f95de0386854f1d3b579fc6c3
Caroline Permansson <caroline.permansson@student.kyh.se>
2016-12-03 16:26:06 +0100

    Log out button on admin page

---
+++ loginAdmin/logout.php
@@ -0,0 +1,17 @@
+<?php 
+error_reporting(E_ALL & ~E_NOTICE);
+session_start();
+session_destroy();
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Logout Page</title>
+ </head>
+ <body>
+ <a href="login.php">Klicka här för att logga ut. return to homepage.</a>
+ </body>
+ </html>
