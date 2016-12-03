1dbab93ea08985649b30b284a4a2e790dcd6c05c
Caroline Permansson <caroline.permansson@student.kyh.se>
2016-12-03 21:02:50 +0100

    MySql injection

--- loginAdmin/connection.php
+++ loginAdmin/connection.php
@@ -1,6 +1,10 @@
 <?php 
 
 $dbCon = mysqli_connect("localhost", "root", "", "relese2login");
+// MySqli injection defense.
+$username = mysqli_real_escape_string($dbCon, $_POST['username']);
+$password = mysqli_real_escape_string($dbCon, $_POST['password']);
+$sql_command = "select * from users where username = '" . $username; $sql_command .= "' AND password = '" . $password . "'";"
 
 if(mysqli_connect_errno()){
 	echo "Failed to connect: " . mysqli_connect_error();
